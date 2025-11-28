7d7260656f66a1d993366885112426faa4845455
Bryan Osmar <bryanosmar07@gmail.com>
2025-11-28 13:07:27 -0500

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from setuptools import find_packages, setup
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="03_Regression_GoldPricePrediction",
+    version="0.0.1",
+    author="bryanOsmar07",
+    author_email="bryanosmar07@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
